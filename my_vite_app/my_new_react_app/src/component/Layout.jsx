d2fa64ae4c7c0b2f791a61be1283ff5887a22692
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-10-15 17:32:22 +0530

    reactadded

--- my_vite_app/my_new_react_app/src/component/Layout.jsx
+++ my_vite_app/my_new_react_app/src/component/Layout.jsx
@@ -1,18 +1,50 @@
-import {Link} from "react-router-dom"
+// import {Link,Outlet} from "react-router-dom"
 
 
-function Layout(){
-    return(
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home page</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+// function Layout(){
+//     return(
+//         <>
+//         <nav>
+//             <ul>
+//                 <li>
+//                     <Link to="/">Home page</Link>
+//                 </li>
+//                 <li>
+//                     <Link to="/contact">Contact</Link>
+//                 </li>
+//             </ul>
+//         </nav>
+//         <Outlet/>
+//         </>
+//     )
+// }
+// export default Layout
+
+
+
+
+
+
+
+import { NavLink, Outlet } from "react-router-dom";
+import './Navbar.css';
+
+function Layout() {
+    return (
+        <>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li>
+                        <NavLink to="/" end activeClassName="active">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </>
+    );
 }
+
 export default Layout
